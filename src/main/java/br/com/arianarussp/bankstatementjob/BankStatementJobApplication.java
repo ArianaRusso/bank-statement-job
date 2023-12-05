b63f3a21d2ff93d64dca99d5549b1c8724fa942c
ArianaRusso <1214Unicamp!>
2023-12-05 12:15:05 -0300

    configiuração data source e estrutura de pacotes

---
+++ src/main/java/br/com/arianarussp/bankstatementjob/BankStatementJobApplication.java
@@ -0,0 +1,13 @@
+package br.com.arianarussp.bankstatementjob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankStatementJobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankStatementJobApplication.class, args);
+	}
+
+}
